780b7ee99a5531cc19ad942de6bb468135318a24
israel <israel.bejarano.a@gmail.com>
2021-04-10 18:23:24 +0200

    levantar el proyecto base ok

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-var package = require('./package.json');
+import PKG from './package.json';
+import Block from './src/blockchain/block';
 
-console.log(`${package.name} v${package.version}`);
+const { name, version } = PKG;
+console.log(`Proyecto ${name} v${version} started`);
+
+const block = new Block(Date.now(), 'pr3vious-h4sh4', 'h4sh', 'd4t4');
+console.log(block.toString());
