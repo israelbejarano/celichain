8fc280d5b5491dac9b57e32091b0f1ed58cd2857
israel <israel.bejarano.a@gmail.com>
2021-04-11 17:49:22 +0200

    test para la funcion validate()

---
+++ src/blockchain/modules/validate.test.js
@@ -0,0 +1,46 @@
+import BlockChain from '../blockchain';
+import validate from './validate';
+describe('validate()', () => {
+    let blockchain;
+
+    beforeEach(() => {
+        blockchain = new BlockChain();
+    });
+
+    it('validates a valid chain', () => {
+        blockchain.addBlock('bloque-1');
+        blockchain.addBlock('bloque-2');
+
+        expect(validate(blockchain.blocks)).toBe(true);
+
+    });
+
+    it('invalidates a chain with corrupt genesis block', () => {
+        blockchain.blocks[0].data = 'bad data';
+
+        expect(() => {
+            validate(blockchain.blocks)
+        }).toThrowError('Invalid Genesis Block.');
+
+    });
+
+    it('invalidates a chain with corrupt previousHash within a block', () => {
+        blockchain.addBlock('bloque-test');
+        blockchain.blocks[1].previousHash = 'hack';
+
+        expect(() => {
+            validate(blockchain.blocks)
+        }).toThrowError('Invalid previous hash.');
+
+    });
+
+    it('invalidates a chain with corrupt hash within a block', () => {
+        blockchain.addBlock('bloque-test');
+        blockchain.blocks[1].hash = 'hack';
+
+        expect(() => {
+            validate(blockchain.blocks)
+        }).toThrowError('Invalid hash.');
+
+    });
+});
