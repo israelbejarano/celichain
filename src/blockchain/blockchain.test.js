2f48a3469ac3df77e7c4bfa954eeb452af71ca79
israel <israel.bejarano.a@gmail.com>
2021-04-11 18:17:35 +0200

    test de replace

--- src/blockchain/blockchain.test.js
+++ src/blockchain/blockchain.test.js
@@ -3,9 +3,11 @@ import Block from './block';
 
 describe('Blockchain', () => {
     let blockchain;
+    let blockchainB;
 
     beforeEach(() => {
         blockchain = new Blockchain();
+        blockchainB = new Blockchain();
     });
 
     it('every blockchain has a genesis blockchain', () => {
@@ -23,5 +25,29 @@ describe('Blockchain', () => {
         const [, lastBlock] = blockchain.blocks;
         expect(lastBlock.data).toEqual(data);
         expect(blockchain.blocks.length).toEqual(2);
+    });
+
+    it('replaces the chain with a valid chain', () => {
+        blockchainB.addBlock('block-1');
+        blockchain.replace(blockchainB.blocks);
+
+        expect(blockchain.blocks).toEqual(blockchainB.blocks);
+    });
+
+    it('does not replace the chain with one with less blocks', () => {
+        blockchain.addBlock('bloque-1');
+
+        expect(() => {
+            blockchain.replace(blockchainB.blocks)
+        }).toThrowError('Received chain is not longer than current chain');
+    });
+
+    it('not replace the chain with one is not valid', () => {
+        blockchainB.addBlock('bloque-1');
+        blockchainB.blocks[1].data = 'block-hack';
+
+        expect(() => {
+            blockchain.replace(blockchainB.blocks)
+        }).toThrowError('Received chain is invalid');
     });
 });
